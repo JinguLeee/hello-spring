04156bb924c12fa86dacfa6a4002224d5b20ace7
JinguLeee <pingu514@naver.com>
2023-07-14 02:25:17 +0900

    스프링 데이터 JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -13,24 +13,24 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    @Autowired // 생성자 하나인 경우 생략 가능
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean // 알아서 빈에 등록
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository(); 코드 주입시 바꿔낄 수 있음
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);    // 실행하지 않아도 통합테스트를 통해 테스트를 할 수 있음
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+////        return new MemoryMemberRepository(); 코드 주입시 바꿔낄 수 있음
+////        return new JdbcMemberRepository(dataSource);
+////        return new JdbcTemplateMemberRepository(dataSource);    // 실행하지 않아도 통합테스트를 통해 테스트를 할 수 있음
+////        return new JpaMemberRepository(em);
+//    }
 
 }
